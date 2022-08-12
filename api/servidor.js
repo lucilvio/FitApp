8156ade2a8f0a573055d7f29fc5e9333926c7827
Vanessa Dias <vanevanessa.dias@gmail.com>
2022-08-12 09:41:10 +0100

    iniciando a construção da api

---
+++ api/servidor.js
@@ -0,0 +1,22 @@
+//importa o modulo express (biblioteca para contrução de api)
+const express = require('express');
+
+//cria uma aplicação express (objeto)
+const app = express();
+
+//cria o frontend do swagger
+const swaggerUi = require('swagger-ui-express');
+const swaggerDoc = require('./swagger.json');
+
+const port = process.env.PORT || 3000;
+
+//inicia o servidor
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
+
+//middlewares
+app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+
+module.exports.app = app;
