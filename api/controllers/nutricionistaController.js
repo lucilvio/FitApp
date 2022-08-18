d1a966fa307690ff340ecf9e9f3defae5128cd40
Vanessa Dias <vanevanessa.dias@gmail.com>
2022-08-18 12:52:43 +0100

    endpoint nutricionistaController

--- api/controllers/nutricionistaController.js
+++ api/controllers/nutricionistaController.js
@@ -3,15 +3,21 @@ const base = require('../dados.js');
 const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
 
+
 function cadastrarNutricionista(req, res) {
-    const token = req.body.token;
-    const usuario = jwt.verify(token, 'shhhhh');
+    const token = req.headers.authorization.replace("Bearer ", "");
+  
+    try{
+        const usuario = jwt.verify(token, 'shhhhh');
+    } catch(erro) {
+        res.status(401).send({ erro: "Erro na validação do Token."})
+    }
 
     if (usuario.perfil !== "administrador") {
-        res.status(401).send("Erro: Acesso não autorizado.");
+        res.status(401).send({erro: "Erro: Acesso não autorizado."});
         return;
     }
-    
+
 
     let novoUsuario = {
         id: crypto.randomUUID(),
@@ -27,7 +33,7 @@ function cadastrarNutricionista(req, res) {
         res.status(400).send({ erro: "Não é possível cadastrar usuário sem email" });
         return;
     }
-        
+
     if (base.dados.usuarios.find(usuario => usuario.login == novoUsuario.login)) {
         res.status(400).send({ erro: "Esse e-mail já foi cadastrado" });
         return;
@@ -35,22 +41,37 @@ function cadastrarNutricionista(req, res) {
 
     let novoNutricionista = {
         id: crypto.randomUUID(),
-        idUsuario: novoUsuario.id,
+        usuario: novoUsuario,
         nome: req.body.nome,
         login: req.body.login,
         telefone: req.body.telefone,
         registroProfissional: req.body.registroProfissional
     }
 
-    base.dados.usuarios.push({ id: novoUsuario.id, nome: novoUsuario.nome, login: novoUsuario.login, senha: novoUsuario.senha, bloqueado: novoUsuario.bloqueado, perfil: novoUsuario.perfil, mensagens: novoUsuario.mensagens});
 
-    base.dados.nutricionistas.push({ id: novoNutricionista.id, idUsuario: novoNutricionista.idUsuario, nome: novoNutricionista.nome, login: novoNutricionista.login, telefone: novoNutricionista.telefone, registroProfissional: novoNutricionista.registroProfissional})
+
+    base.dados.usuarios.push({ id: novoUsuario.id, nome: novoUsuario.nome, login: novoUsuario.login, senha: novoUsuario.senha, bloqueado: novoUsuario.bloqueado, perfil: novoUsuario.perfil, mensagens: novoUsuario.mensagens });
+
+    base.dados.nutricionistas.push({ id: novoNutricionista.id, idUsuario: novoNutricionista.idUsuario, nome: novoNutricionista.nome, login: novoNutricionista.login, telefone: novoNutricionista.telefone, registroProfissional: novoNutricionista.registroProfissional })
 
 
     res.send(base.dados.nutricionistas);
 
 }
 
+function buscarNutricionistas(req, res) {
+    const nome = req.query.nome;
+    const nutricionistas = base.dados.nutricionistas;
+
+    res.send(nutricionistas)
+}
 
 
-exports.cadastrarNutricionista = cadastrarNutricionista;
+
+module.exports = {
+    cadastrarNutricionista: cadastrarNutricionista,
+    buscarNutricionistas: buscarNutricionistas
+}
+
+
+
--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const servidor = require('./servidor');
 const loginController = require('./controllers/loginController');
 const nutricionistaController = require('./controllers/nutricionistaController');
 
+
 servidor.app.post('/login', loginController.login);
 servidor.app.post('/nutricionista', nutricionistaController.cadastrarNutricionista);
-servidor.app.get('/usuarios/:perfil', usuariosController.buscarUsuarios);
+servidor.app.get('/nutricionista', nutricionistaController.buscarNutricionistas);
